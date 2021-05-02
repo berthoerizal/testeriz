dd68cb499066f88ea2f414406d7e9fb02ecb8f2a
Bertho Erizal <berthoerizal21@gmail.com>
2021-05-03 00:12:00 +0700

    login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,89 +1,179 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ asset('assets/login/style.css') }}" />
-    <title>Register</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TESTERIZ-REGISTER</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #dee9ff;
+        }
+
+        .registration-form {
+            padding: 50px 0;
+        }
+
+        .registration-form form {
+            background-color: #fff;
+            max-width: 600px;
+            margin: auto;
+            padding: 50px 70px;
+            border-top-left-radius: 30px;
+            border-top-right-radius: 30px;
+            box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.075);
+        }
+
+        .registration-form .form-icon {
+            text-align: center;
+            background-color: #5891ff;
+            border-radius: 50%;
+            font-size: 40px;
+            color: white;
+            width: 100px;
+            height: 100px;
+            margin: auto;
+            margin-bottom: 50px;
+            line-height: 100px;
+        }
+
+        .registration-form .item {
+            border-radius: 20px;
+            margin-bottom: 25px;
+            padding: 10px 20px;
+        }
+
+        .registration-form .create-account {
+            border-radius: 30px;
+            padding: 10px 20px;
+            font-size: 18px;
+            font-weight: bold;
+            background-color: #5791ff;
+            border: none;
+            color: white;
+            margin-top: 20px;
+        }
+
+        .registration-form .social-media {
+            max-width: 600px;
+            background-color: #fff;
+            margin: auto;
+            padding: 35px 0;
+            text-align: center;
+            border-bottom-left-radius: 30px;
+            border-bottom-right-radius: 30px;
+            color: #9fadca;
+            border-top: 1px solid #dee9ff;
+            box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.075);
+        }
+
+        .registration-form .social-icons {
+            margin-top: 30px;
+            margin-bottom: 16px;
+        }
+
+        .registration-form .social-icons a {
+            font-size: 23px;
+            margin: 0 3px;
+            color: #5691ff;
+            border: 1px solid;
+            border-radius: 50%;
+            width: 45px;
+            display: inline-block;
+            height: 45px;
+            text-align: center;
+            background-color: #fff;
+            line-height: 45px;
+        }
+
+        .registration-form .social-icons a:hover {
+            text-decoration: none;
+            opacity: 0.6;
+        }
+
+        @media (max-width: 576px) {
+            .registration-form form {
+                padding: 50px 20px;
+            }
+
+            .registration-form .form-icon {
+                width: 70px;
+                height: 70px;
+                font-size: 30px;
+                line-height: 70px;
+            }
+        }
+
+    </style>
 </head>
 
 <body>
-    <div class="container">
-        <div class="forms-container">
-            <div class="signin-signup">
-                <form method="POST" class="sign-in-form" action="{{ route('register') }}">
-                    @csrf
-                    <h2 class="title">Register</h2>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input id="name" type="text" name="name" value="{{ old('name') }}" required
-                            class="@error('name') is-invalid @enderror" placeholder="Nama Lengkap" autofocus>
-                    </div>
-                    @error('name')
-                        <br>
-                        <span class="invalid-feedback" role="alert">
-                            <strong style="color: red;">{{ $message }}</strong>
-                        </span>
-                        <br>
-                    @enderror
-                    <div class="input-field">
-                        <i class="fas fa-envelope"></i>
-                        <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email"
-                            value="{{ old('email') }}" placeholder="Email" required>
-                    </div>
-                    @error('email')
-                        <br>
-                        <span class="invalid-feedback" role="alert">
-                            <strong style="color: red;">{{ $message }}</strong>
-                        </span>
-                        <br>
-                    @enderror
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input id="password" type="password" class="@error('password') is-invalid @enderror"
-                            name="password" placeholder="Password" required>
-                    </div>
-                    @error('password')
-                        <br>
-                        <span class="invalid-feedback" role="alert">
-                            <strong style="color: red;">{{ $message }}</strong>
-                        </span>
-                        <br>
-                    @enderror
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
-                            required autocomplete="new-password" placeholder="Konfirmasi Password">
-                    </div>
-                    <button type="submit" class="btn solid">
-                        {{ __('Register') }}
-                    </button>
-                </form>
+    <div class="registration-form">
+        <form method="POST" class="sign-in-form" action="{{ route('register') }}">
+            @csrf
+            <div class="form-icon">
+                <span><i class="icon icon-user"></i></span>
             </div>
-        </div>
 
-        <div class="panels-container">
-            <div class="panel left-panel">
-                <div class="content">
-                    <h3>Sudah punya akun ?</h3>
-                    <p>
-                        <a href="{{ url('/') }}" style="text-decoration:none; color: white;"><b>TESTERIZ</b></a>
-                        adalah
-                        sistem yang menyediakan fungsi atau fitur yang biasanya dibutuhkan dalam pembuatan website
-                        dengan framework Laravel.
-                    </p>
-                    <a href="{{ route('login') }}"><button class="btn btn-outline-primary"
-                            id="sign-up-btn">Login</button>
-                    </a>
+            @error('name')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
                 </div>
-                <img src="{{ asset('assets/login/img/log.svg') }}" class="image" alt="" />
-            </div>
-        </div>
-    </div>
+            @enderror
+            @error('email')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
+            @enderror
+            @error('password')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
+            @enderror
 
-    <script src="{{ asset('assets/login/app.js') }}"></script>
+            <div class="form-group">
+                <input class="form-control item" id="name" type="text" name="name" value="{{ old('name') }}"
+                    class="@error('name') is-invalid @enderror" placeholder="Nama Lengkap" autofocus>
+            </div>
+            <div class="form-group">
+                <input class="form-control item" id="email" type="text" class="@error('email') is-invalid @enderror"
+                    name="email" value="{{ old('email') }}" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control item" id="password"
+                    class="@error('password') is-invalid @enderror" name="password" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <input id="password-confirm" type="password" class="form-control item" name="password_confirmation"
+                    autocomplete="new-password" placeholder="Konfirmasi Password">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-block create-account">
+                    {{ __('Register') }}
+                </button>
+            </div>
+            <p>
+                Kamu sudah punya akun? <a href="{{ route('login') }}" class="btn btn-link">Login Sekarang!
+                </a>
+            </p>
+        </form>
+    </div>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js">
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#birth-date').mask('00/00/0000');
+            $('#phone-number').mask('0000-0000');
+        })
+
+    </script>
 </body>
 
 </html>
